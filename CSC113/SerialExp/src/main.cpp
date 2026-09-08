ce7c590891c5a13552455df2ab975ca39fe97c65
Ryan Chen <wigwomm@gmail.com>
2026-09-08 11:27:54 -0400

    testing serials

--- CSC113/SerialExp/src/main.cpp
+++ CSC113/SerialExp/src/main.cpp
@@ -5,10 +5,11 @@ int myFunction(int, int);
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("waiting for key");
 }
 
 void loop(){
-  if(Serial.available()){
+  if(Serial.available()>0){
     String test = Serial.readStringUntil('\n'); 
     Serial.println(test);
   }
